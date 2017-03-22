23b962d7916dbfe3396980c229bb2dcf3b1e6246
NN <nn4work@gmail.com>
2017-03-23 00:03:46 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -42,6 +42,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入成功！"
+    else
+      flash[:waring] = "你已經是成員了"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出"
+    else
+      flash[:warning] = "你不是成員 怎麼退出"
+    end
+
+    redirect_to group_path(@group)
+  end
+
   private
 
   def find_group_and_check_permission
